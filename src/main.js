9bc9b435db4e5c67ae79e9f85f497e8e4ff6025c
makun <etukobamasatyan@gmail.com>
2021-12-01 16:12:26 +0900

    googleadsenceコード追加

--- src/main.js
+++ src/main.js
@@ -6,4 +6,10 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css' // add
 import 'bootstrap-vue/dist/bootstrap-vue.css' // add
 
+import Ads from 'vue-google-adsense'
+
+App.use(require('vue-script2'))
+
+App.use(Ads.AutoAdsense, { adClient: 'ca-pub-8270225967881362' })
+
 createApp(App).use(store).use(router).mount('#app')
