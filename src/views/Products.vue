fa875fad0810951b79c13afc17c60a7e9645a3d2
makun <etukobamasatyan@gmail.com>
2021-12-25 23:21:32 +0900

    vueでv-bindでsrc指定すると画像表示されへんのがなおせん

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,12 +1,29 @@
 <template>
 
 <a>Products List</a>
-  <div class="row flex-row flex-nowrap overflow-auto">
-    
-        <div class="card card-block h-100" >
-          <img class = "logo" src = "@/assets/hakadori.png">
-          <p>ハカドリくん</p>
+
+
+<div class = "container">
+  <div class="card" v-for="idea in ideas" v-bind:key="idea">
+  <div class="card-body">
+        <div class="row toprow" >
+          <div class="col">
+            <img class = "logo" src = {{syasin}}>
+          </div>
+            <div class = "col">
+          <p>{{idea.name}}</p>
         </div>
+      </div>
+      <div class="row underrow">
+        <div class="col">
+          <img class = "git" src="@/assets/logo_github.png">
+        </div>
+        <div class="col">
+          <a href="https://github.com/mercy34mercy/hakadorikun"></a>
+        </div>
+      </div>
+      </div>
+  </div>
 </div>
 </template>
 
@@ -19,6 +36,8 @@ export default {
   data: () => ({
     windowWidth: window.innerWidth,
     windowHeight: window.innerHeight,
+    ideas:[{name:"Beautifulgirl",src:require("@/assets/logo_github.png")},{name:"ハカドリくん",src:require("@/assets/logo_github.png")}],
+    syasin: require("@/assets/logo_github.png")
   }),
   methods: {
     getWindowSize: function () {
@@ -34,12 +53,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.row{
+.card {
   height: 400px;
 }
 
-.logo{
-  width:30%;
+.logo {
+  width: 50%;
   padding: 5%;
+}
+
+.row{
+  height: 50%;
+}
+
+.git{
+  height: 30%;
 }
 </style>
