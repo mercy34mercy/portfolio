ee2a6cdc47141f495fbd218dbd555dd8d4d32eea
mercy34mercy <masakun@DESKTOP-PLC0L1Q.localdomain>
2022-04-19 14:36:22 +0900

    productpage修正

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,30 +1,32 @@
 <template>
+  <p class="title">Products List</p>
 
-<a>Products List</a>
-
-
-<div class = "container">
-  <div class="card" v-for="idea in ideas" v-bind:key="idea">
-  <div class="card-body">
-        <div class="row toprow" >
+  <div class="container">
+    <div class="card" v-for="idea in ideas" v-bind:key="idea">
+      <div class="card-body">
+        <div class="row toprow">
           <div class="col">
-            <img class = "logo" src = {{syasin}}>
+            <a :href=idea.url><img class="logo" :src="idea.src" /></a>
           </div>
-            <div class = "col">
-          <p>{{idea.name}}</p>
+          <div class="col">
+            <p class="ideaname">{{ idea.name }}</p>
+          </div>
+        </div>
+        <div class="row underrow">
+          <div class="col">
+            <div class="row"></div>
+            <div class="row">
+              <a :href=idea.githuburl><img class="git" src="@/assets/logo_github.png" /></a>
+            </div>
+          </div>
+          <div class="col">
+            <div class="row"></div>
+            <div class="row"></div>
+          </div>
         </div>
       </div>
-      <div class="row underrow">
-        <div class="col">
-          <img class = "git" src="@/assets/logo_github.png">
-        </div>
-        <div class="col">
-          <a href="https://github.com/mercy34mercy/hakadorikun"></a>
-        </div>
-      </div>
-      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -36,8 +38,12 @@ export default {
   data: () => ({
     windowWidth: window.innerWidth,
     windowHeight: window.innerHeight,
-    ideas:[{name:"Beautifulgirl",src:require("@/assets/logo_github.png")},{name:"ハカドリくん",src:require("@/assets/logo_github.png")}],
-    syasin: require("@/assets/logo_github.png")
+    ideas: [
+      { name: "ハカドリくん", src: require("../assets/iconhakadorikun.png"),url:"https://play.google.com/store/apps/details?id=jp.masashi.hakadori",githuburl:"https://github.com/mercy34mercy/hakadorikun" },
+      { name: "banmeshi", src: require("../assets/banmeshi.png"),url:"https://banmeshikun.com/",githuburl:"https://github.com/mercy34mercy/banmeshi_vue" },
+      { name: "京都通り名タイピング", src: require("../assets/kyototyping.png"),url:"https://kyoto-typing.net/",githuburl:"https://github.com/mercy34mercy/Reacttyping" },
+    ],
+    syasin: require("@/assets/logo_github.png"),
   }),
   methods: {
     getWindowSize: function () {
@@ -54,19 +60,34 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  height: 400px;
+  height: 20rem;
+}
+
+.title{
+  font-size: 3rem;
 }
 
 .logo {
-  width: 50%;
+  height: 10em;
   padding: 5%;
 }
 
-.row{
+.row {
   height: 50%;
 }
 
-.git{
-  height: 30%;
+.git {
+  margin-left: 5em;
+  padding: 1em;
+  height: 6em;
+
+}
+
+.ideaname {
+  font-size: 2em;
+}
+
+.row_ {
+  height: 50%;
 }
 </style>
