5056b54f04e3adc3f426d57d42bd698e5fe8f639
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-04-20 22:33:51 +0900

    productlist内容更新

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -6,7 +6,7 @@
       <div class="card-body">
         <div class="row toprow">
           <div class="col">
-            <a :href=idea.url><img class="logo" :src="idea.src" /></a>
+            <a :href="idea.url"><img class="logo" :src="idea.src" /></a>
           </div>
           <div class="col">
             <p class="ideaname">{{ idea.name }}</p>
@@ -16,12 +16,15 @@
           <div class="col">
             <div class="row"></div>
             <div class="row">
-              <a :href=idea.githuburl><img class="git" src="@/assets/logo_github.png" /></a>
+              <div class="logogit">
+                <a :href="idea.githuburl"
+                  ><img class="git" src="@/assets/logo_github.png"
+                /></a>
+              </div>
             </div>
           </div>
           <div class="col">
-            <div class="row"></div>
-            <div class="row"></div>
+            {{ idea.coment }}
           </div>
         </div>
       </div>
@@ -39,9 +42,28 @@ export default {
     windowWidth: window.innerWidth,
     windowHeight: window.innerHeight,
     ideas: [
-      { name: "ハカドリくん", src: require("../assets/iconhakadorikun.png"),url:"https://play.google.com/store/apps/details?id=jp.masashi.hakadori",githuburl:"https://github.com/mercy34mercy/hakadorikun" },
-      { name: "banmeshi", src: require("../assets/banmeshi.png"),url:"https://banmeshikun.com/",githuburl:"https://github.com/mercy34mercy/banmeshi_vue" },
-      { name: "京都通り名タイピング", src: require("../assets/kyototyping.png"),url:"https://kyoto-typing.net/",githuburl:"https://github.com/mercy34mercy/Reacttyping" },
+      {
+        name: "ハカドリくん",
+        src: require("../assets/iconhakadorikun.png"),
+        url: "https://play.google.com/store/apps/details?id=jp.masashi.hakadori",
+        githuburl: "https://github.com/mercy34mercy/hakadorikun",
+        coment:
+          "はかどりくんは、大学の課題の管理がしにくいそんな悩みを解決するアンドロイドアプリです",
+      },
+      {
+        name: "banmeshi",
+        src: require("../assets/banmeshi.png"),
+        url: "https://banmeshikun.com/",
+        githuburl: "https://github.com/mercy34mercy/banmeshi_vue",
+        coment: "banmeshiは自炊をしたいけど何を作るか決めるのがめんどくさい!というお悩みを解決するアプリです",
+      },
+      {
+        name: "京都通り名タイピング",
+        src: require("../assets/kyototyping.png"),
+        url: "https://kyoto-typing.net/",
+        githuburl: "https://github.com/mercy34mercy/Reacttyping",
+        coment: "京都の通り名をタイピングで覚えちゃおうという楽しいゲームです",
+      },
     ],
     syasin: require("@/assets/logo_github.png"),
   }),
@@ -63,29 +85,33 @@ export default {
   height: 20rem;
 }
 
-.title{
+.title {
   font-size: 3rem;
+  
 }
 
 .logo {
   height: 10em;
   padding: 5%;
+  max-width: 15em;
 }
 
+.logogit{
+  margin:0 auto;
+}
 
 .row {
   height: 50%;
 }
 
 .git {
-  margin-left: 5em;
   padding: 1em;
   height: 6em;
-
 }
 
 .ideaname {
   font-size: 2em;
+  font-weight: bold;
 }
 
 .row_ {
